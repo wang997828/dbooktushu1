bb5a27d0e324ba634afe0d9dff2313e9bfe9be24
ming1223 <981223lihaoming>
2018-12-09 15:14:18 +0800

    xiugai

--- src/main/java/com/oracledp/book/biz/Adminbiz.java
+++ src/main/java/com/oracledp/book/biz/Adminbiz.java
@@ -3,5 +3,10 @@ package com.oracledp.book.biz;
 import com.oracledp.book.model.Admin;
 
 public interface Adminbiz {
+    /**
+     * xiugai
+     * @param admin
+     * @return
+     */
     boolean adminbizPadandName(Admin admin);
 }
